e93f13271acb7062ef2592ed5d088da7f63f2557
Linda Le <yueling1222@gmail.com>
2020-04-07 00:48:09 -0600

    Update test for internal api call

--- user_food_preference_test.py
+++ user_food_preference_test.py
@@ -1,27 +1,30 @@
 import requests
 import json
 import pdb
-from base import app
 from flask import Flask
 from instance import config
-
-def test_can_connect_to_api_endpoint():
-    client = app.test_client()
-    response = client.get('/api/v1/recommendations')
-
-    assert response.data == b'Hello, World!'
+from base import app
 
 def test_user_selects_food_preference_returns_successful_response():
+    client = app.test_client()
+
     headers = { 'authorization': config.YELP_API_KEY }
     lat = '39.7392358'
     long = '-104.990251'
     cuisine = 'indian'
-    params = { 'latitude': lat, 'longitude': long, 'term': cuisine }
-    url = "https://api.yelp.com/v3/businesses/search?latitude=% s&longitude=% s&term=% s"%(lat, long, cuisine)
-
-    response = requests.get(url, headers=headers)
+    params = { 'latitude': lat, 'longitude': long, 'categories': cuisine }
+    response = client.get("/api/v1/recommendations?latitude=% s&longitude=% s&categories=% s"%(lat, long, cuisine))
+    response_body = json.loads(response.data.decode('utf8'))
 
     assert response.status_code == 200
+    assert 'id' in response_body['data']
+    assert 'cuisine' in response_body['data']['attributes']
+    assert 'display_phone' in response_body['data']['attributes']
+    assert 'location' in response_body['data']['attributes']
+    assert 'name' in response_body['data']['attributes']
+    assert 'phone' in response_body['data']['attributes']
+    assert 'price' in response_body['data']['attributes']
+    assert 'rating' in response_body['data']['attributes']
 
 def test_user_selects_food_preference_returns_restaurant_data():
     headers = { 'authorization': config.YELP_API_KEY }
