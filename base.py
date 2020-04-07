0c859f2e67249ac92b4e5785cbc5b37e67c88c9b
Linda Le <yueling1222@gmail.com>
2020-04-07 00:48:42 -0600

    Add updated base app file

--- base.py
+++ base.py
@@ -1,21 +1,29 @@
-from flask import Flask
+from flask import Flask, request
+import json
 import requests
+import random
 from instance import config
+from restaurant import Restaurant
+from restaurant_schema import RestaurantSchema
+from marshmallow_jsonapi import Schema, fields
 
 app = Flask(__name__)
 
 @app.route('/api/v1/recommendations', methods=['GET'])
 def index():
     headers = { 'authorization': config.YELP_API_KEY }
-    lat = '39.7392358'
-    long = '-104.990251'
-    cuisine = 'indian'
-    params = { 'latitude': lat, 'longitude': long, 'term': cuisine }
+    lat = request.args['latitude']
+    long = request.args['longitude']
+    cuisine = request.args['categories']
+    params = {'latitude': f"{lat}", 'longitude': f"{long}", 'term': f"{cuisine}"}
     url = "https://api.yelp.com/v3/businesses/search"
-
     response = requests.get(url, params=params, headers=headers)
-
-    return 'Hello, World!'
+    data = json.dumps(response.json())
+    restaurants = Restaurant.from_json(data)
+    restaurant = random.choice(restaurants)
+    schema = RestaurantSchema()
+    result = schema.dump(restaurant)
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
