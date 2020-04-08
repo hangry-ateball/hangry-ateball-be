c09cf5c0ca7c9faea240c7892b8f2a55c04522c4
Scott Regenthal <scottregenthal@gmail.com>
2020-04-08 13:31:32 -0600

    Move respective files into base_app and tests directories
    Co-Authored-By: Linda Le <yueling1222@gmail.com>
    Co-Authored-By: Jonathan Patterson <jonpatt92@gmail.com>

---
+++ base_app/restaurant_schema.py
@@ -0,0 +1,15 @@
+from marshmallow_jsonapi import Schema, fields
+
+class RestaurantSchema(Schema):
+    id = fields.Str()
+    metadata = fields.ResourceMeta()
+    cuisine = fields.List(fields.Str)
+    name = fields.Str()
+    location = fields.Str()
+    phone = fields.Str()
+    display_phone = fields.Str()
+    rating = fields.Float()
+    price = fields.Str()
+
+    class Meta:
+        type_ = 'restaurants'
