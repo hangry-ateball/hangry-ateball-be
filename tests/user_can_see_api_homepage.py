45c983fea809cb4a03eb2e84f02ababb587cd7a4
Linda Le <yueling1222@gmail.com>
2020-04-08 17:31:34 -0600

    Add tests for homepage route

--- tests/user_can_see_api_homepage.py
+++ tests/user_can_see_api_homepage.py
@@ -2,13 +2,15 @@ import os
 import requests
 from base_app.base import app
 
-def test_user_selects_food_preference_returns_successful_response():
-    lat = '39.7392358'
-    long = '-104.990251'
-    cuisine = 'indian'
-    url = '/api/v1/recommendations?latitude={}&longitude={}&categories={}'.format(lat, long, cuisine)
-
+def test_homepage_returns_successful_response():
+    url = '/'
     client = app.test_client()
     response = client.get(url)
-
     assert response.status_code == 200
+
+def test_homepage_returns_documentation_info():
+    url = '/'
+    client = app.test_client()
+    response = client.get(url)
+    text = response.data
+    assert text == 'Welcome to the Hangry-8Ball API'
