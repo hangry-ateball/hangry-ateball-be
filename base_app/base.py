7cfe49790b97a0e3f61a54943ea39cde241e0b34
Scott Regenthal <scottregenthal@gmail.com>
2020-04-09 10:52:06 -0600

    Add route to get photos from restaurant corresponding to business ID

--- base_app/base.py
+++ base_app/base.py
@@ -5,6 +5,8 @@ import requests
 from flask import Flask, request
 from base_app.restaurant import Restaurant
 from base_app.restaurant_schema import RestaurantSchema
+from base_app.photo import Photo
+from base_app.photo_schema import PhotoSchema
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -33,6 +35,34 @@ def index():
     restaurant = random.choice(restaurants)
     schema = RestaurantSchema()
     result = schema.dump(restaurant)
+
+    return result
+
+@app.route('/api/v1/photos', methods=['GET'])
+def get_photos():
+    YELP_API_KEY = os.getenv('YELP_API_KEY')
+    headers = {'authorization': 'Bearer ' + YELP_API_KEY}
+    lat = request.args['latitude']
+    long = request.args['longitude']
+    cuisine = request.args['categories']
+    params = {'latitude': f'{lat}', 'longitude': f'{long}', 'term': f'{cuisine}'}
+    url = 'https://api.yelp.com/v3/businesses/search'
+
+    response = requests.get(url, params=params, headers=headers)
+    json_data = json.dumps(response.json())
+    restaurants = Restaurant.from_json(json_data)
+    restaurant = random.choice(restaurants)
+    schema = RestaurantSchema()
+    result = schema.dump(restaurant)
+
+    restaurant_id = result['data']['id']
+    restaurant_url = f'https://api.yelp.com/v3/businesses/{restaurant_id}'
+
+    response = requests.get(restaurant_url, headers=headers)
+    json_data = json.dumps(response.json())
+    photos = Photo.from_json(json_data)
+    schema = PhotoSchema()
+    result = schema.dump(photos)
 
     return result
 
