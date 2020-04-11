ca5db68f7cad34a2e4967f05da3e5d0577e3e108
Jon Patterson <JonPatt92@gmail.com>
2020-04-11 17:03:26 -0600

    Create tests for YelpService and its methods to verify proper functionality

---
+++ tests/yelp_service_test.py
@@ -0,0 +1,43 @@
+from base_app.yelp_service import YelpService
+from dotenv import load_dotenv
+load_dotenv()
+
+def test_can_initialize():
+    service = YelpService()
+    assert isinstance(service, YelpService)
+
+def test_can_connect():
+    url = 'https://api.yelp.com/v3/businesses/search'
+    params = {'latitude': '39.7392358', 'longitude': '-104.990251'}
+
+    service = YelpService()
+    response = service.connection(url, params)
+
+    assert response.status_code == 200
+    assert 'businesses' in response.json()
+
+def test_can_get_recommendation():
+    params = {'latitude': '39.7392358', 'longitude': '-104.990251'}
+
+    service = YelpService()
+    response = service.get_recommendation(params)
+
+    assert 'id' in response['data']
+    assert 'cuisine' in response['data']['attributes']
+    assert 'name' in response['data']['attributes']
+    assert 'location' in response['data']['attributes']
+    assert 'phone' in response['data']['attributes']
+    assert 'display_phone' in response['data']['attributes']
+    assert 'rating' in response['data']['attributes']
+    assert 'price' in response['data']['attributes']
+
+def test_can_get_photos():
+    params = {'latitude': '39.7392358', 'longitude': '-104.990251'}
+
+    service = YelpService()
+    restaurant = service.get_recommendation(params)
+    response = service.get_photos(restaurant)
+
+    assert 'id' in response['data']
+    assert 'type' in response['data']
+    assert 'photos' in response['data']['attributes']
