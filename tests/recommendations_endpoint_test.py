82fb28f7e826301d70f824532d0cfa0676fed48a
Jon Patterson <JonPatt92@gmail.com>
2020-04-11 16:28:53 -0600

    Add tests for the Price preference to verify correct logic function

--- tests/recommendations_endpoint_test.py
+++ tests/recommendations_endpoint_test.py
@@ -15,6 +15,7 @@ def test_user_selects_multiple_preferences_returns_successful_response():
     assert response.status_code == 200
 
     response_body = response.json
+    price = len(response_body['data']['attributes']['price'])
 
     assert 'id' in response_body['data']
     assert 'cuisine' in response_body['data']['attributes']
@@ -24,6 +25,7 @@ def test_user_selects_multiple_preferences_returns_successful_response():
     assert 'display_phone' in response_body['data']['attributes']
     assert 'rating' in response_body['data']['attributes']
     assert 'price' in response_body['data']['attributes']
+    assert price <= 2
 
 def test_user_selects_food_preference_returns_successful_response():
     lat = '39.7392358'
@@ -50,7 +52,7 @@ def test_user_selects_food_preference_returns_successful_response():
 def test_user_selects_price_preference_returns_successful_response():
     lat = '39.7392358'
     long = '-104.990251'
-    price = '2'
+    price = '3'
 
     url = '/api/v1/recommendations?latitude={}&longitude={}&price={}'.format(lat, long, price)
     client = app.test_client()
@@ -66,5 +68,68 @@ def test_user_selects_price_preference_returns_successful_response():
     assert 'display_phone' in response_body['data']['attributes']
     assert 'rating' in response_body['data']['attributes']
     assert 'price' in response_body['data']['attributes']
+
+def test_user_selects_price_preference_1_returns_successful_response():
+    lat = '39.7392358'
+    long = '-104.990251'
+    price = '1'
+
+    url = '/api/v1/recommendations?latitude={}&longitude={}&price={}'.format(lat, long, price)
+    client = app.test_client()
+    response = client.get(url)
+    assert response.status_code == 200
+
+    response_body = response.json
+    price = len(response_body['data']['attributes']['price'])
+    assert 'price' in response_body['data']['attributes']
+    assert price == 1
+
+def test_user_selects_price_preference_2_returns_successful_response():
+    lat = '39.7392358'
+    long = '-104.990251'
+    price = '2'
+
+    url = '/api/v1/recommendations?latitude={}&longitude={}&price={}'.format(lat, long, price)
+    client = app.test_client()
+    response = client.get(url)
+    assert response.status_code == 200
+
+    response_body = response.json
+    price = len(response_body['data']['attributes']['price'])
+    assert 'price' in response_body['data']['attributes']
+    assert price <= 2
+
+def test_user_selects_price_preference_3_returns_successful_response():
+    lat = '39.7392358'
+    long = '-104.990251'
+    price = '3'
+
+    url = '/api/v1/recommendations?latitude={}&longitude={}&price={}'.format(lat, long, price)
+    client = app.test_client()
+    response = client.get(url)
+    assert response.status_code == 200
+
+    response_body = response.json
+    price = len(response_body['data']['attributes']['price'])
+    assert 'price' in response_body['data']['attributes']
+    assert price <= 3
+
+def test_user_selects_price_preference_4_returns_successful_response():
+    lat = '39.7392358'
+    long = '-104.990251'
+    price = '4'
+
+    url = '/api/v1/recommendations?latitude={}&longitude={}&price={}'.format(lat, long, price)
+    client = app.test_client()
+    response = client.get(url)
+    assert response.status_code == 200
+
+    response_body = response.json
+    price = len(response_body['data']['attributes']['price'])
+    assert 'price' in response_body['data']['attributes']
+    assert price == 4
+
+
+
 
 
