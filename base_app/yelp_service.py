14d64287b4e87e144d01e23909431ee5dc316390
Jon Patterson <JonPatt92@gmail.com>
2020-04-11 15:08:15 -0600

    Abstract connection to Yelp API into `connection` method which can be passed the url and params needed for establishing a connection with Yelp API and return unformatted response

--- base_app/yelp_service.py
+++ base_app/yelp_service.py
@@ -10,15 +10,16 @@ load_dotenv()
 class YelpService:
 
     def get_recommendation(self, params):
-        YELP_API_KEY = os.getenv('YELP_API_KEY')
-        headers = {'authorization': 'Bearer ' + YELP_API_KEY}
         url = 'https://api.yelp.com/v3/businesses/search'
-
-        response = requests.get(url, params=params, headers=headers)
-
+        response = self.connection(url, params)
         json_data = json.dumps(response.json())
         restaurants = Restaurant.from_json(json_data)
         restaurant = random.choice(restaurants)
         schema = RestaurantSchema()
         result = schema.dump(restaurant)
         return result
+
+    def connection(self, url, params):
+        YELP_API_KEY = os.getenv('YELP_API_KEY')
+        headers = {'authorization': 'Bearer ' + YELP_API_KEY}
+        return requests.get(url, params=params, headers=headers)
