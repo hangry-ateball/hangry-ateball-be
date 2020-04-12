5c0819350c0261d6ba98e76520f684d2b3a93c07
Linda Le <yueling1222@gmail.com>
2020-04-11 23:45:12 -0600

    Add test that checks coordinates returned with city and state

--- tests/google_service_test.py
+++ tests/google_service_test.py
@@ -19,7 +19,7 @@ def test_google_api_returns_successful_response():
     status = response.status_code
     assert response.status_code == 200
 
-def test_google_api_returns_coordinates():
+def test_google_api_returns_coordinates_with_street_address():
     GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
     address = '1331 17th St LL100, Denver, CO'
     params = {'key': GOOGLE_API_KEY, 'address': f'{address}' }
@@ -29,3 +29,14 @@ def test_google_api_returns_coordinates():
     response = response.json()
     assert response['results'][0]['geometry']['location']['lat'] == 39.7508006
     assert response['results'][0]['geometry']['location']['lng'] == -104.9965947
+
+def test_google_api_returns_coordinates_with_city_and_state():
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+    address = 'Denver, CO'
+    params = {'key': GOOGLE_API_KEY, 'address': f'{address}' }
+    url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    service = GoogleService()
+    response = service.connection(url, params)
+    response = response.json()
+    assert response['results'][0]['geometry']['location']['lat'] == 39.7392358
+    assert response['results'][0]['geometry']['location']['lng'] == -104.990251
