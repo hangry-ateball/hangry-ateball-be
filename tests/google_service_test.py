edf8ae74b8e75f3ba1b07dc8a16ea7d4a16b331e
Linda Le <yueling1222@gmail.com>
2020-04-11 23:16:14 -0600

    Add google service test

---
+++ tests/google_service_test.py
@@ -0,0 +1,10 @@
+import os
+import requests
+from base_app.base import app
+from dotenv import load_dotenv
+load_dotenv()
+
+def test_google_api_returns_successful_response():
+
+
+def test_google_api_returns_coordinates():
