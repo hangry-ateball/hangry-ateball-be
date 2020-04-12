321106f41fbda4437a5de4f2356585e3d00d2e72
Linda Le <yueling1222@gmail.com>
2020-04-11 23:33:15 -0600

    Add test to check GoogleService initializes

--- tests/google_service_test.py
+++ tests/google_service_test.py
@@ -1,16 +1,21 @@
 import os
 import requests
 from base_app.base import app
+from base_app.google_service import GoogleService
 from dotenv import load_dotenv
 load_dotenv()
 
+def test_can_initialize():
+    service = GoogleService()
+    assert isinstance(service, GoogleService)
+
 def test_google_api_returns_successful_response():
     GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
-    url = https://maps.googleapis.com/
-    params = {'key': GOOGLE_API_KEY }
-    url = 'https://maps.googleapis.com/maps/api/geocode/json?address=#{}'.format()
-    response = requests.get(url, params=params)
+    address = '1331 17th St LL100, Denver, CO'
+    params = {'key': GOOGLE_API_KEY, 'address': f'{address}' }
+    url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    service = GoogleService()
+    response = service.connection(url, params)
     status = response.status_code
     assert response.status_code == 200
 
-def test_google_api_returns_coordinates():
