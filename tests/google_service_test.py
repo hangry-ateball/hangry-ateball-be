07ef0ab4a97855bee5f142291e063a1cc01592fa
Linda Le <yueling1222@gmail.com>
2020-04-11 23:21:00 -0600

    Add variables and get request to google service test

--- tests/google_service_test.py
+++ tests/google_service_test.py
@@ -5,6 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def test_google_api_returns_successful_response():
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+    url = https://maps.googleapis.com/
+    params = {'key': GOOGLE_API_KEY }
+    url = 'https://maps.googleapis.com/maps/api/geocode/json?address=#{}'.format()
 
 
 def test_google_api_returns_coordinates():
