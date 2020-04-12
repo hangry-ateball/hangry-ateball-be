b228c19f582adc52a40fa1cc4cb8c2aa02585494
Linda Le <yueling1222@gmail.com>
2020-04-11 23:41:40 -0600

    Add test to ensure GoogleService returns coordinates

--- tests/google_service_test.py
+++ tests/google_service_test.py
@@ -19,3 +19,13 @@ def test_google_api_returns_successful_response():
     status = response.status_code
     assert response.status_code == 200
 
+def test_google_api_returns_coordinates():
+    GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
+    address = '1331 17th St LL100, Denver, CO'
+    params = {'key': GOOGLE_API_KEY, 'address': f'{address}' }
+    url = 'https://maps.googleapis.com/maps/api/geocode/json'
+    service = GoogleService()
+    response = service.connection(url, params)
+    response = response.json()
+    assert response['results'][0]['geometry']['location']['lat'] == 39.7508006
+    assert response['results'][0]['geometry']['location']['lng'] == -104.9965947
