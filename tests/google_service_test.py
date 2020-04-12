487412684c51f7756231ecd49146432bf4d26a35
Linda Le <yueling1222@gmail.com>
2020-04-11 23:21:59 -0600

    Add line to assert status code is 200

--- tests/google_service_test.py
+++ tests/google_service_test.py
@@ -9,6 +9,8 @@ def test_google_api_returns_successful_response():
     url = https://maps.googleapis.com/
     params = {'key': GOOGLE_API_KEY }
     url = 'https://maps.googleapis.com/maps/api/geocode/json?address=#{}'.format()
-
+    response = requests.get(url, params=params)
+    status = response.status_code
+    assert response.status_code == 200
 
 def test_google_api_returns_coordinates():
